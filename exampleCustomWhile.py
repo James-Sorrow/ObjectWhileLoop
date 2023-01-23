1e4520d01a192b07ce2e087e8d52d353a73a4605
James <105584897+James-Sorrow@users.noreply.github.com>
2023-01-22 21:07:03 -0600

    Added exampleCustomWhile.py

---
+++ exampleCustomWhile.py
@@ -0,0 +1,22 @@
+from WhileObjects import While
+
+#Custom while loop overriding step function's calc interface
+class customLoop(While):
+
+    def step(self):
+        if self.op == "<" or self.op == "<=":
+            if not self.isFinished():
+                self.calc += self.i
+                self.i += self.increment
+        elif self.op == ">" or self.op == ">=":
+            if not self.isFinished():
+                self.calc += self.i
+                self.i -= self.increment            
+        return self.i
+
+
+loop = customLoop(i=0,end=10)
+while not loop.isFinished():
+    loop.step()
+
+print(loop.calc)
